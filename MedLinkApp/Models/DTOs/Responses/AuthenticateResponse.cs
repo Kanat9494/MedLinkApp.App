6a01dc08c61a9424817021c558c808a6c5231c41
kana2022 <kkanat94@gmail.com>
2023-03-22 11:24:36 +0600

    ending

--- MedLinkApp/Models/DTOs/Responses/AuthenticateResponse.cs
+++ MedLinkApp/Models/DTOs/Responses/AuthenticateResponse.cs
@@ -5,4 +5,5 @@ public class AuthenticateResponse : BaseResponse
     public int UserId { get; set; }
     public string UserName { get; set; }
     public string UserEmail { get; set; }
+    public string AccessToken { get; set; }
 }
