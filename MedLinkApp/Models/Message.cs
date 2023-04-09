750ccfb82a5d4f9939b7c763ab3b6ec5b0a00288
Kanat9494 <kkanat94@gmail.com>
2023-04-09 12:02:02 +0600

    1

--- MedLinkApp/Models/Message.cs
+++ MedLinkApp/Models/Message.cs
@@ -5,8 +5,6 @@ internal class Message
     public string SenderName { get; set; }
     public string ReceiverName { get; set; }
     public string Content { get; set; }
-    public byte[] AudioMessage { get; set; }
     public string AudioUrl { get; set; }
-    public byte[] Image { get; set; }
     public string ImageUrl { get; set; }
 }
