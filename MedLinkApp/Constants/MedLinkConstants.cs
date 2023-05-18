e79a6092bf0357e166f01f524be9a45a991aa066
kana2022 <kkanat94@gmail.com>
2023-05-18 11:22:38 +0600

    18052023_2

--- MedLinkApp/Constants/MedLinkConstants.cs
+++ MedLinkApp/Constants/MedLinkConstants.cs
@@ -3,10 +3,10 @@
 public class MedLinkConstants
 {
     //расскомментировать для телефона на работе
-    internal const string SERVER_ROOT_URL = "http://localhost:45455";
+    //internal const string SERVER_ROOT_URL = "http://localhost:45455";
 
     //работает для эмулятора
-    //internal const string SERVER_ROOT_URL = "http://192.168.2.33:45455";
+    internal const string SERVER_ROOT_URL = "http://192.168.2.33:45455";
 
     //для дома
     //internal const string SERVER_ROOT_URL = "http://192.168.1.51:45455";
