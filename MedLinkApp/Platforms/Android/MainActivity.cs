1ae758d52af9d937342194e5ff4511ca4ef278e9
kana2022 <kkanat94@gmail.com>
2023-05-19 17:13:53 +0600

    19052023_14

--- MedLinkApp/Platforms/Android/MainActivity.cs
+++ MedLinkApp/Platforms/Android/MainActivity.cs
@@ -7,16 +7,16 @@ namespace MedLinkApp;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
-    protected override void OnCreate(Bundle savedInstanceState)
-    {
-        base.OnCreate(savedInstanceState);
+    //protected override void OnCreate(Bundle savedInstanceState)
+    //{
+    //    base.OnCreate(savedInstanceState);
 
-        UserActivityTrackService.Instance.StartSession();
-    }
-    public override void OnUserInteraction()
-    {
-        base.OnUserInteraction();
+    //    UserActivityTrackService.Instance.StartSession();
+    //}
+    //public override void OnUserInteraction()
+    //{
+    //    base.OnUserInteraction();
 
-        UserActivityTrackService.Instance.RestartSession();
-    }
+    //    UserActivityTrackService.Instance.RestartSession();
+    //}
 }
