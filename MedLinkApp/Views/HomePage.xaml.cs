cb10321aa5ef6c0cb7b58a3d11e9a26b1573f3e0
Kanat9494 <kkanat94@gmail.com>
2023-09-26 23:10:22 +0600

    1.0.14

--- MedLinkApp/Views/HomePage.xaml.cs
+++ MedLinkApp/Views/HomePage.xaml.cs
@@ -41,16 +41,13 @@ public partial class HomePage : ContentPage
                         {
                             new Border
                             {
-                                Stroke = Color.FromArgb("#C49B33"),
-                                Background = Color.FromArgb("#2B0B98"),
-                                StrokeThickness = 4,
-                                Padding = new Thickness(16, 8),
-                                HorizontalOptions = LayoutOptions.Center,
+                                Stroke = Colors.Transparent,
+                                Background = Color.FromArgb("#ACACAC"),
+                                HorizontalOptions = LayoutOptions.FillAndExpand,
                                 StrokeShape = new RoundRectangle
                                 {
-                                    CornerRadius = new CornerRadius(40, 0, 0, 40)
+                                    CornerRadius = new CornerRadius(6, 6, 6, 6)
                                 },
-                                WidthRequest = 150,
                                 HeightRequest = 25
                             },
 
