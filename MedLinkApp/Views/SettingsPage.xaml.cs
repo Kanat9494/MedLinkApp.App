6007b63108c51d68841f214eeff27215db98fedb
kana2022 <kkanat94@gmail.com>
2023-10-02 14:39:00 +0600

    1.0.63

--- MedLinkApp/Views/SettingsPage.xaml.cs
+++ MedLinkApp/Views/SettingsPage.xaml.cs
@@ -17,6 +17,11 @@ public partial class SettingsPage : ContentPage
     {
         App.Current.Dispatcher.Dispatch(async () =>
         {
+            contentSL.Add(new Image
+            {
+                Aspect = Aspect.AspectFill
+            }.Source("https://picsum.photos/id/1/200/300").Height(400).Margins(0, 0, 0, 25));
+
             await Task.Delay(1000);
 
             contentSL.Add(new StackLayout
